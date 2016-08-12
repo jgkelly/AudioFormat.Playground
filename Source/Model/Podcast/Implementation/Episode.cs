3b8565721eb89958f5fd31c6eb64a065c6898d45
CORP\jeff.kelly <jeff.kelly@spok.com>
2016-08-12 14:52:15 -0400

    Initial updates to solution

---
+++ Source/Model/Podcast/Implementation/Episode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Mp4Chapters;
+using Podcast.Interfaces;
+
+namespace Podcast.Implementation
+{
+    public class Episode : IEpisode
+    {
+        public Episode(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("path Parameter can not be null or empty.");
+            }
+
+            GetChapters(path);
+        }
+
+        public string Title { get; }
+        public string Subtitle { get; }
+        public string Description { get; }
+        public string Link { get; }
+        public string UniqueId { get; }
+        public string AlbumArt { get; }
+        public bool Explicit { get; }
+        public DateTime Published { get; }
+        public TimeSpan Duration { get; }
+        public IList<string> Keywords { get; }
+        public IList<IChapter> Chapters { get; private set; }
+
+        private void GetChapters(string episode)
+        {
+            using (var inputStream = File.OpenRead(episode))
+            {
+                var extractor = new ChapterExtractor(new StreamWrapper(inputStream));
+                extractor.Run();
+                foreach (ChapterInfo chapterInfo in extractor.Chapters)
+                {
+                    Console.WriteLine("{0} : {1}", chapterInfo.Time, chapterInfo.Name);
+                }
+            }
+        }
+    }
+}
---
+++ Source/Model/Podcast/Interfaces/IEpisode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Podcast.Interfaces
+{
+    /// <summary>
+    /// Represents a single podcast episode
+    /// </summary>
+    public interface IEpisode
+    {
+        string Title { get; }
+        string Subtitle { get; }
+        string Description { get; }
+        string Link { get; }
+        string UniqueId { get; }
+        string AlbumArt { get; }
+        bool Explicit { get; }
+        DateTime Published { get; }
+        TimeSpan Duration { get; }
+        IList<string> Keywords { get; }
+        IList<IChapter> Chapters { get; }
+    }
+}
